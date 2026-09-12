d0c880a454ab68798fbb3a822bd3af468702888a
sam-thomas-dev <samsjournies@gmail.com>
2026-09-12 12:19:40 +1000

    Commited by repo agent.

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-test code comment
+// test code comment
 'use strict';
 
 const Busboy = require('busboy');
-
-/*
- * This module will parse the multipart-form containing files and fields from the lambda event object.
- * @param {event} - an event containing the multipart-form in the body
- * @return {object} - a JSON object containing array of files and fields, sample below.
-    {
-        files: [
-            {
-                filename: 'test.pdf',
-                content: <Buffer 25 50 6f 62 ... >,
-                contentType: 'application/pdf',
-                encoding: '7bit',
-                fieldname: 'uploadFile1'
-            }
-        ],
-        field1: 'VALUE1',
         field2: 'VALUE2',
 	
 file_path: index.js
